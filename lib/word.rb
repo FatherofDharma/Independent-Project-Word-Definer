61028d86034c39c5c967ea0c9ea34d9228ae3e43
FatherofDharma <fatherofdharma@gmail.com>
2019-07-26 09:17:45 -0700

    add a method to return all words in the class variable words.

--- lib/word.rb
+++ lib/word.rb
@@ -11,4 +11,12 @@ class Word
     @id = id || @@total_ids +=1
   end
 
+  def self.all()
+    @@words.values()
+  end
+
+  # def == (word_to_be_compared)
+  #   self.name() == word_to_be_compared.name()
+  # end
+
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -10,4 +10,26 @@ describe '#Word' do
     end
   end
 
+  describe('.all') do
+    it('returns and empty array where future words will be stored') do
+      expect(Word.all).to(eq([]))
+    end
+  end
+
+  # describe('#save') do
+  #   it('store a word object in a hash, mocking a database') do
+  #     word = Word.new('bubblefy', nil)
+  #     word.save()
+  #     expect(Word.name).to(eq('bubblefy'))
+  #   end
+  # end
+
+  # describe('#==') do
+  #   it('will identify as the same word if it has matching attributes') do
+  #     word = Word.new('bubblefy', nil)
+  #     word2 = Word.new('bubblefy', nil)
+  #     expect().to(eq())
+  #   end
+  # end
+
 end
