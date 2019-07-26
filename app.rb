4272aed6790e6d3716e19e73bee92f5f80ca67ce
FatherofDharma <fatherofdharma@gmail.com>
2019-07-26 14:41:30 -0700

    add paths to word page, edit word page, and return to main page.

--- app.rb
+++ app.rb
@@ -20,11 +20,28 @@ get ('/words/new') do
   erb(:add_word)
 end
 
+get ('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+end
+
 post ('/words') do
   name = params[:new_word]
   word = Word.new(name, nil)
   word.save()
 
+  @words = Word.sort()
+  erb(:words)
+end
+
+get ('/words/:id/edit') do
+  @word = Word.find(params[:id].to_i())
+  erb(:edit_word)
+end
+
+patch ('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.edit(params[:name])
   @words = Word.sort()
   erb(:words)
 end
