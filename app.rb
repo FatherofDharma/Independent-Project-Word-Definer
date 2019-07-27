37225c7f27705212ef7a60f54725261946bc5365
FatherofDharma <fatherofdharma@gmail.com>
2019-07-26 19:19:05 -0700

    fix error in app.rb missing require statement for definition file.

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/word')
-# require('./lib/definition')
+require('./lib/definition')
 require('pry')
 also_reload('lib/**/*.rb')
 also_reload('app.rb')
