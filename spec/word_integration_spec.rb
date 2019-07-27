98f672f8b3386a509d068f671a0ba396282ea860
FatherofDharma <fatherofdharma@gmail.com>
2019-07-26 18:02:30 -0700

    add integration spec for deleting a definition and passed.

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -62,3 +62,15 @@ describe('edit a definition', {:type => :feature}) do
     expect(page).to have_content('to lick something until your tongue falls off')
   end
 end
+
+describe('delete a definition', {:type => :feature}) do
+  it('deletes a definition') do
+    word = Word.new('lickify', nil)
+    word.save()
+    definition = Definition.new('to cover something in tongue juice', word.id, nil)
+    definition.save()
+    visit("/words/#{word.id}/definitions/#{definition.id}")
+    click_on('Delete Definition!')
+    expect(page).to have_no_content('to cover something in tongue juice')
+  end
+end
